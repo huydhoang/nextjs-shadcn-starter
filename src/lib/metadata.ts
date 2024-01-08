8000a399ef8690269dc115c564e6ac61c990a882
huydhoang <huydhoan9@gmail.com>
2024-01-08 20:30:47 +0700

    Initial commit

---
+++ src/lib/metadata.ts
@@ -0,0 +1,7 @@
+export const siteMetadata = {
+  title: 'MyApp - the marketplace for local communities',
+  description:
+    'MyApp is a marketplace for high-quality local goods and services.',
+  creator: '@huydh',
+  url: 'https://example.com',
+}
